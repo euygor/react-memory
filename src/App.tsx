a44946aeb9c838654628f398bf5916841615a749
euygor <ygor.oliveiragb@gmail.com>
2022-04-04 00:00:41 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,147 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import * as C from './App.styles';
 
-function App() {
+import logoImage from './assets/devmemory_logo.png';
+import RestartIcon from './svgs/restart.svg';
+
+import { InfoItem } from './components/InfoItem';
+import { Button } from './components/Button';
+import { GridItem } from './components/GridItem';
+
+import { GridItemType } from './types/GridItemType';
+import { items } from './data/items';
+
+import { formatTimeElapsed } from './helpers/formatTimeElapsed';
+
+const App = () => {
+  const [playing, setPlaying] = useState<boolean>(false);
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [moveCount, setMoveCount] = useState<number>(0);
+  const [shownCount, setShownCount] = useState<number>(0);
+  const [gridItems, setGridItems] = useState<GridItemType[]>([]);
+
+  useEffect(() => {
+    resetAndCreateGrid();
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (playing) {
+        setTimeElapsed(timeElapsed + 1);
+      }
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [playing, timeElapsed]);
+
+  useEffect(() => {
+    if (shownCount === 2) {
+      let opened = gridItems.filter(item => item.shown === true);
+
+      if (opened.length === 2) {
+        if (opened[0].item === opened[1].item) {
+          let tempGrid = [...gridItems];
+          for (let i in tempGrid) {
+            if (tempGrid[i].shown) {
+              tempGrid[i].permanentShown = true;
+              tempGrid[i].shown = false;
+            }
+          }
+          setGridItems(tempGrid);
+          setShownCount(0);
+        } else {
+          setTimeout(() => {
+            let tempGrid = [...gridItems];
+            for (let i in tempGrid) {
+              if (tempGrid[i].shown) {
+                tempGrid[i].shown = false;
+              }
+            }
+            setGridItems(tempGrid);
+            setShownCount(0);
+          }, 1000);
+        }
+
+        setMoveCount(moveCount => moveCount + 1);
+      }
+    }
+  }, [shownCount, gridItems]);
+
+  useEffect(() => {
+    if (moveCount > 0 && gridItems.every(item => item.permanentShown === true)) {
+      setPlaying(false);
+    }
+  }, [moveCount, gridItems]);
+
+  const resetAndCreateGrid = () => {
+    // 1 - reset game
+    setTimeElapsed(0);
+    setMoveCount(0);
+    setShownCount(0);
+
+    // 2 - create grid
+    let tempGrid: GridItemType[] = [];
+    for (let i = 0; i < (items.length * 2); i++) {
+      tempGrid.push({
+        item: null,
+        shown: false,
+        permanentShown: false
+      });
+    }
+
+    // 2.1 - popular grid
+    for (let x = 0; x < 2; x++) {
+      for (let y = 0; y < items.length; y++) {
+        let position = -1;
+        while (position < 0 || tempGrid[position].item !== null) {
+          position = Math.floor(Math.random() * (items.length * 2));
+        }
+        tempGrid[position].item = y;
+      }
+    }
+
+    // 2.2 - set state
+    setGridItems(tempGrid);
+
+    // 3 start game
+    setPlaying(true);
+  }
+
+  const handleItemClick = (index: number) => {
+    if (playing && index !== null && shownCount < 2) {
+      let tempGrid = [...gridItems];
+
+      if (!tempGrid[index].permanentShown && !tempGrid[index].shown) {
+        tempGrid[index].shown = true;
+        setShownCount(shownCount + 1);
+      }
+      setGridItems(tempGrid);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href="">
+          <img src={logoImage} width={200} alt="" />
+        </C.LogoLink>
+
+        <C.InfoArea>
+          <InfoItem label="Tempo" value={formatTimeElapsed(timeElapsed)} />
+          <InfoItem label="Movimentos" value={moveCount.toString()} />
+        </C.InfoArea>
+
+        <Button label="Reniciar" icon={RestartIcon} onClick={resetAndCreateGrid} />
+      </C.Info>
+      <C.GridArea>
+        <C.Grid>
+          {gridItems.map((item, index) => {
+            return (
+              <GridItem key={index} item={item} onClick={() => handleItemClick(index)} />
+            );
+          })}
+        </C.Grid>
+      </C.GridArea>
+    </C.Container>
   );
 }
 
